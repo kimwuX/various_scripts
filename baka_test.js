57f96f63bcea0e48799daae3ffd1d17fe479cde8
kimwu2009 <kimwu2009@gmail.com>
2024-02-17 09:25:02 +0800

    baka_test

--- baka_test.js
+++ baka_test.js
@@ -23,32 +23,36 @@
     //反序列化表单数据
     function deserializeFormData(data) {
         var formData = {};
-        
+
         data.split('&').forEach(function(item) {
             var keyValuePair = item.split('=');
-            
+
             if (!keyValuePair[0]) return;
-            
+
             var key = decodeURIComponent(keyValuePair[0]);
             var value = decodeURIComponent(keyValuePair[1] || '');
-            
+
             formData[key] = value;
         });
-        
+
         return formData;
     }
 
     function isValidDate(date) {
-        return date instanceof Date && !isNaN(date.getTime())
+        return date instanceof Date && !isNaN(date.getTime());
+    }
+
+    function isEmptyString(str) {
+        return !(str != null && str.length > 0);
     }
 
     function padStart(value, len, fill) {
         len = len || 2;
         fill = fill || '0';
-        return value.toString().padStart(len, fill)
+        return value.toString().padStart(len, fill);
     }
 
-    function getLeftTime(value) {
+    function getLeftTimeString(value) {
         let h = Math.floor(value / 1000 / 60 / 60);
         let m = Math.floor(value / 1000 / 60 % 60);
         let s = Math.floor(value / 1000 % 60);
@@ -62,7 +66,7 @@
         if (isValidDate(dt)) {
             dt.setFullYear(now.getFullYear());
             //console.log('pgtime: ' + dt);
-            console.log(getLeftTime(now - dt) + ' faster than server.');
+            console.log(getLeftTimeString(now - dt) + ' faster than server.');
             return (now - dt) / 1000;
         }
         return 0;
@@ -77,15 +81,63 @@
             next.setDate(next.getDate() + 1);
         }
         if (offset) {
-            next.setSeconds(next.getSeconds() + offset);
+            //next.setSeconds(next.getSeconds() + offset);
+            next.setMilliseconds(next.getMilliseconds() + offset * 1000);
         }
         return next;
     }
 
-    function signCHD() {
+    function nextListTime(arr, offset) {
 
-        //刷题
-        let training = false;
+        let next;
+        let now = new Date();
+        for (let i = 0; i < arr.length; i++) {
+            if (now.getHours() < arr[i]) {
+                next = nextTime(arr[i], 0, 0, offset);
+                break;
+            }
+        }
+        if (next == null && arr.length > 0) {
+            next = nextTime(arr[0], 0, 0, offset);
+        }
+        console.log('nextListTime: ' + next);
+        return next;
+    }
+
+    function startTicktock(tick) {
+        setInterval(() => {
+            console.log(new Date().toLocaleTimeString());
+        }, tick);
+    }
+
+    //防止休眠
+    function playAudio() {
+
+        var source = document.createElement('source');
+        source.src = 'https://open.cd/attachments/202210/202210010005258775518ae99535515641dbecb5a3113e.mp3';
+        //source.src = 'https://open.cd/attachments/202210/20221001000532b0ff1a4c576fc75bdaadef6c9c5e4409.mp3';
+        //source.src = 'https://open.cd/attachments/202210/20221001000536089c29811d3a2f08045aa4185c32e475.mp3';
+        source.type = 'audio/mpeg';
+
+        let audio = new Audio();
+        audio.appendChild(source);
+        audio.style = 'width: 100%';
+        audio.controls = true;
+        audio.autoplay = true;
+        //audio.muted = true;
+        audio.loop = true;
+
+        let td = document.createElement('td');
+        td.appendChild(audio);
+
+        let tr = document.createElement('tr');
+        tr.appendChild(td);
+
+        $('td#nav_block').parent().after(tr);
+    }
+
+    function signCHD(training) {
+
         let delay = 0;
         if (training) {
             if ($("*:contains('签到记录')").length > 0) {
@@ -115,7 +167,7 @@
         console.log(dic_chd);
         let id = $('input[name="questionid"]').val();
         //console.log(id);
-        if (id == null || id.length == 0) {
+        if (isEmptyString(id)) {
             return;
         }
         let a = dic_chd[id];
@@ -154,10 +206,8 @@
         }
     }
 
-    function signTJU() {
+    function signTJU(training) {
 
-        //刷题
-        let training = false;
         let delay = 0;
         if (training) {
             //阻止提交表单
@@ -169,7 +219,7 @@
             delay += 3000 * (0.5 + Math.random());
             console.log('delay: ' + delay);
         } else {
-            if ($("*:contains('签到记录')").length > 0) {
+            if ($("*:contains('签到记录')").length > 0 || $("*:contains('验证码错误')").length > 0) {
                 document.location.reload();
                 return;
             }
@@ -180,22 +230,27 @@
             dic_tju = JSON.parse(GM_getValue('dic_tju'));
         }
         console.log(dic_tju);
-        let id = $('table.captcha').find('img').prop('src').split('/').pop();
+        let id;
+        try {
+            id = $('table.captcha').find('img').prop('src').split('/').pop();
+        } catch (error) {
+            console.log(error);
+        }
         console.log(id);
-        if (id == null || id.length == 0) {
+        if (isEmptyString(id)) {
             return;
         }
         let a = dic_tju[id];
 
         $('input[name="submit"]').click(function() {
             //console.log('click.');
-            a = a || [];
+            a = [];
             $('input[name="ban_robot"]').each(function() {
                 if ($(this).prop('checked') == true) {
-                    let v = $(this).val().split('&').pop();
-                    if (v && a.indexOf(v) == -1) {
-                        a.push(v);
-                    }
+                    //let v = $(this).val().split('&').pop();
+                    //if (v && a.indexOf(v) == -1) {
+                    //    a.push(v);
+                    //}
                     let n = $(this).parent().text();
                     if (n && a.indexOf(n) == -1) {
                         a.push(n);
@@ -229,10 +284,36 @@
         }
     }
 
-    function signU2(partial) {
+    function delay_signTJU() {
 
-        //刷题
-        let training = false;
+        let now = new Date();
+        //let offset = correctTime($('span#datetime').text()) || 1;
+        //let next = nextTime(8, 0, 0, 1.5);
+        let arr = [6, 7, 8, 12, 18, 20, 22];
+        let next = nextListTime(arr, 1.5);
+        console.log('now: ' + now);
+        console.log('next: ' + next);
+        let val = next - now;
+        console.log(getLeftTimeString(val) + ' left.');
+        if (val <= 0) {
+            signTJU();
+        } else if (val < 90000) {
+            setTimeout(() => {
+                signTJU();
+            }, val);
+            startTicktock(1000);
+        } else {
+            setTimeout(() => {
+                document.location.reload();
+            }, val - 90000);
+            startTicktock(300000);
+        }
+        signOthers();
+        playAudio();
+    }
+
+    function signU2(training, partial) {
+
         let delay = 0;
         if (training) {
             if (!partial) {
@@ -251,7 +332,7 @@
                             //observer.disconnect();
                             setTimeout(function() {
                                 //console.clear();
-                                signU2(true);
+                                signU2(training, true);
                             }, 500);
                         }
                     }
@@ -341,81 +422,46 @@
             setTimeout(() => {
                 win.close();
                 openSites(arr);
-            }, 20000);
+            }, 30000);
         } else {
             console.log('openSites done: ' + new Date());
         }
     }
 
-    function startTicktock(tick) {
-        setInterval(() => {
-            console.log(new Date().toLocaleTimeString());
-        }, tick);
-    }
+    function signOthers() {
 
-    //防止休眠
-    function playAudio() {
+        let now = new Date();
+        let tt = nextTime(6, 20 * Math.random(), 0);
+        console.log('openSites1: ' + tt);
+        setTimeout(() => {
+            console.log('openSites1 begin: ' + new Date());
+            openSites(site_list.slice());
+        }, tt - now);
 
-        var source = document.createElement('source');
-        source.src = 'https://open.cd/attachments/202210/202210010005258775518ae99535515641dbecb5a3113e.mp3';
-        //source.src = 'https://open.cd/attachments/202210/20221001000532b0ff1a4c576fc75bdaadef6c9c5e4409.mp3';
-        //source.src = 'https://open.cd/attachments/202210/20221001000536089c29811d3a2f08045aa4185c32e475.mp3';
-        source.type = 'audio/mpeg';
-
-        let audio = new Audio();
-        audio.appendChild(source);
-        audio.style = 'width: 100%';
-        audio.controls = true;
-        audio.autoplay = true;
-        //audio.muted = true;
-        audio.loop = true;
-
-        let td = document.createElement('td');
-        td.appendChild(audio);
-
-        let tr = document.createElement('tr');
-        tr.appendChild(td);
-
-        $('td#nav_block').parent().after(tr);
+        tt = nextTime(22, 20 * Math.random(), 0);
+        console.log('openSites2: ' + tt);
+        setTimeout(() => {
+            console.log('openSites2 begin: ' + new Date());
+            openSites(site_list.slice());
+        }, tt - now);
     }
 
     setTimeout(function() {
 
+        //刷题
+        let training = false;
         console.log('baka_test.');
         let host = location.host;
         if (host.search(/ptchdbits/i) != -1) {
-            signCHD();
+            signCHD(training);
         } else if (host.search(/tjupt/i) != -1) {
-            let now = new Date();
-            let tt = nextTime(6, 30 * Math.random(), 0);
-            console.log('openSites: ' + tt);
-            setTimeout(() => {
-                console.log('openSites begin: ' + now);
-                openSites(site_list.slice());
-            }, tt - now);
-
-            //let offset = correctTime($('span#datetime').text()) || 1;
-            let next = nextTime(8, 0, 0, 1);
-            console.log('now: ' + now);
-            console.log('next: ' + next);
-            let val = next - now;
-            console.log(getLeftTime(val) + ' left.');
-            if (val <= 0) {
-                signTJU();
-            } else if (val < 60000) {
-                setTimeout(() => {
-                    signTJU();
-                }, val);
-                startTicktock(1000);
+            if (training) {
+                signTJU(true);
             } else {
-                setTimeout(() => {
-                    document.location.reload();
-                }, val - 60000);
-                startTicktock(60000);
+                delay_signTJU();
             }
-            playAudio();
         } else if (host.search(/dmhy/i) != -1) {
-            signU2();
+            signU2(training);
         }
 
     }, 1000);
